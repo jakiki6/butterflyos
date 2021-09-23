1787f2b480d7c83c14c7c13623418e224367df48
Jakob Kirsch <jakob.kirsch@web.de>
2021-09-23 21:39:45 +0200

    vm: loader fix

--- kern/vm.asm
+++ kern/vm.asm
@@ -1,4 +1,22 @@
 	bits 32
 	org 0x200000
 
-_start:	jmp $
+start:	mov esp, 0x1f0000
+
+	mov esi, kernel_file
+
+	; check magic
+	lodsd
+	cmp eax, 0x6f6a6262
+	jne error
+
+	jmp $
+
+error:	lidt [.fake_idt]
+	int 0x69
+.fake_idt:
+	dw 0
+	dd 0
+
+kernel_file:
+	incbin "main.o"
