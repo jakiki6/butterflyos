601c6b315f61c80a82144e0b7f3903927b790263
Jakob Kirsch <jakob.kirsch@web.de>
2021-09-23 22:42:49 +0200

    loader: add it

--- kern/vm.asm
+++ kern/vm.asm
@@ -10,6 +10,57 @@ start:	mov esp, 0x1f0000
 	cmp eax, 0x6f6a6262
 	jne error
 
+	; number of sections
+	lodsd
+	push eax
+
+.section:
+	; origin
+	lodsd
+	mov edi, eax
+
+	; length of binary
+	lodsd
+	mov ecx, eax
+
+	; move
+	rep movsb
+
+	; skip over relocations since they're not relevant for out kernel
+	lodsd
+	shl eax, 2
+	add esi, eax
+
+	; read symbols and panic if we encounter a 0x00 type
+	lodsd
+	push eax
+
+.syms:	; read type
+	lodsb
+
+	; check type
+	cmp al, 0x00
+	je error
+
+	; skip over address
+	lodsd
+
+	; skip over name
+.sn:	lodsb
+	cmp al, 0x00
+	jne .sn
+
+	; loop
+	pop eax
+	push eax
+
+	dec eax
+	cmp eax, 0
+	jne .syms
+
+	; drop value on the stack
+	pop eax
+
 	jmp $
 
 error:	lidt [.fake_idt]
