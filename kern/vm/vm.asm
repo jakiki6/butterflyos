5c8d18d27b46f807d0af715ca6f07fc7685eac1b
Jakob Kirsch <jakob.kirsch@web.de>
2021-10-25 21:33:50 +0200

    vm: save work

--- kern/vm/vm.asm
+++ kern/vm/vm.asm
@@ -1,4 +1,4 @@
-	bits 32
+	bits 64
 	org 0x200000
 
 SERIAL_PORT: \
@@ -6,21 +6,28 @@ SERIAL_PORT: \
 DISABLE_TRACE: \
 	equ 0x202000
 
+MASK_RS: \
+	equ 0x01
+MASK_ALT: \
+	eq 0x02
+MASK_FLOAT: \
+	eq 0x04
+
 %macro push_ps 1
-	sub ebp, 4
-	mov dword [ebp], %1
+	sub rbp, 8
+	mov qword [rbp], %1
 %endmacro
 %macro pop_ps 1
-	mov %1, dword [ebp]
-	add ebp, 4
+	mov %1, qword [rbp]
+	add rbp, 8
 %endmacro
 %macro push_rs 1
-        sub edi, 4
-	mov dword [edi], %1
+        sub rdi, 8
+	mov dword [rdi], %1
 %endmacro
 %macro pop_rs 1
-        mov %1, dword [edi]
-        add edi, 4
+        mov %1, dword [rdi]
+        add rdi, 8
 %endmacro
 
 %macro outb 2
@@ -35,104 +42,109 @@ DISABLE_TRACE: \
 
 start:	jmp $
 
-	mov esp, 0x1f0000
+	mov rsp, 0x1f0000
 
 	call init_serial
 
-	mov esi, kernel_file
+	mov rsi, kernel_file
 
 	; check magic
-	lodsd
-	cmp eax, 0x6f626d65
+	lodsq
+	cmp rax, 0x6f626d65
 	jne error
 
 	; entry
-	lodsd
-	mov dword [entry], eax
+	lodsq
+	mov qword [entry], rax
 
 	; skip over number of sections
-	lodsd
+	lodsq
 
 	; origin
-	lodsd
-	mov edi, eax
+	lodsq
+	mov rdi, rax
 
 	; length of binary
-	lodsd
-	mov ecx, eax
+	lodsq
+	mov rcx, rax
 
 	; move
 	rep movsb
 
 vm:	; registers:
-	; esi=pc ebp=ps edi=rs edx=bp
+	; rsi=pc r8=ps r9=rs r10=bp r11=flags
 
 	; setup registers
-	mov esi, dword [entry]
+	mov rsi, dword [entry]
 
 .main:	cmp byte [DISABLE_TRACE], 1
 	je .notrace
 
-	xchg eax, esi
+	xchg rax, rsi
 	call write_hex
-	xchg eax, esi
+	xchg rax, rsi
 .notrace:
 
 	; clean up
-	cmp byte [.flag_rs], 1
+	test r11, MASK_RS
 	jne .flags
-	xchg ebp, edi
+	xchg r8, r9
 
-.flags:	; clean up the flag
-	mov byte [.flag_rs], 0
+.flags:	; clean up flags
+	and r11, 0xffffffffffffff00
 
 	lodsb
-	test al, 0b10000000
-	jne .execute
-	mov byte [.flag_rs], 1
-	xchg ebp, edi
+
+.isr:	test al, 0b10000000
+	jne .isa
+	or r11, MASK_RS
+	xchg r8, r9
+.isa:	test al, 0b01000000
+	jne .isf
+	or r11, MASK_ALT
+.isf:	test al, 0b00100000
+	jne .nois
+	or r11, MASK_FLOAT
+.nois:
 
 .execute:
 	; mask out function
 	and al, 0b11111
 
 	; get pointer into function table
-	xor ebx, ebx
+	xor rbx, rbx
 	mov bl, al
-	shl ebx, 2
-	add ebx, func_table
-	mov ebx, dword [ebx]
+	shl rbx, 3
+	add rbx, func_table
+	mov rbx, dword [rbx]
 
 	; call it
 	push .main
-	push ebx
+	push rbx
 	ret
 
-.flag_rs:
-	db 0
-
-eqtoeax:
-	; convert equal flag to eax
+eqtorax:
+	; convert equal flag to rax
 	jne .no
-	mov eax, 1
+	mov rax, 1
 	ret
-.no:	xor eax, eax
+.no:	xor rax, rax
 	ret
 
-belowtoeax:  
-        ; convert below flag to eax
+belowtorax:
+        ; convert below flag to rax
         jnb .no   
-        mov eax, 1
+        mov rax, 1
         ret
-.no:    xor eax, eax
+.no:    xor rax, rax
         ret
 
-abovetoeax:  
-        ; convert above flag to eax
+abovetorax:
+        ; convert above flag to rax
         jna .no   
-        mov eax, 1
+        mov rax, 1
         ret
-.no:    xor eax, eax
+.no:    xor rax, rax
         ret
 
 error:	lidt [.fake_idt]
@@ -160,21 +172,21 @@ init_serial:
 	ret
 
 write_serial:
-	push edx
+	push rdx
 
-	push eax
+	push rax
 .cw:	inb SERIAL_PORT + 5
 	and al, 0x20
 	jz .cw
-	pop eax
+	pop rax
 
 	outb SERIAL_PORT + 0, al
 
-	pop edx
+	pop rdx
 	ret
 
 read_serial:
-	push edx
+	push rdx
 
 .cr:	inb SERIAL_PORT + 5
 	and al, 0x01
@@ -182,89 +194,101 @@ read_serial:
 
 	inb SERIAL_PORT + 0
 
-	pop edx
+	pop rdx
 	ret
 
 write_hex:
-	pushad
-	xchg eax, ebx
+	push rax
+	push rbx
+	push rcx
 
-	mov ecx, 4
-.loop:	mov eax, ebx
-	shr eax, 24
+	xchg rax, rbx
+
+	mov rcx, 8
+.loop:	mov rax, rbx
+	shr rax, 24
 	call write_hex_byte
-	shl ebx, 8
+	shl rbx, 8
 	loop .loop
 
 	mov al, 0x0a
 	call write_serial
 
-	popad
+	pop rcx
+	pop rbx
+	pop rax
 	ret
 
 write_hex_byte:
-	pushad
-	xchg edx, eax
+	push rax
+	push rbx
+	push rcx
+	push rdx
 
-	xor ebx, ebx
+	xchg rdx, rax
+
+	xor rbx, rbx
 	mov bl, dl
 	shr bl, 4
-	add ebx, .table
-	mov al, byte [ebx]
+	add rbx, .table
+	mov al, byte [rbx]
 	call write_serial
 
-	xor ebx, ebx
+	xor rbx, rbx
 	mov bl, dl
 	and bl, 0x0f
-	add ebx, .table
-	mov al, byte [ebx]
+	add rbx, .table
+	mov al, byte [rbx]
         call write_serial
 
-	popad
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
 	ret
 .table:	db "0123456789abcdef"
 
 kernel_file:
 	incbin "main.o"
 
-entry:	dd 0
+entry:	dq 0
 
 func_table:
-        dd func_nop     ; 0b00000
-        dd func_lit     ; 0b00001
-        dd func_sstack  ; 0b00010
-        dd func_drop    ; 0b00011
-        dd func_dup     ; 0b00100
-        dd func_swap    ; 0b00101
-        dd func_over    ; 0b00110
-        dd func_rot     ; 0b00111
-        dd func_eq      ; 0b01000
-        dd func_not     ; 0b01001
-        dd func_gth     ; 0b01010
-        dd func_lth     ; 0b01011
-        dd func_jmp     ; 0b01100
-        dd func_jmpc    ; 0b01101
-        dd func_call    ; 0b01110
-        dd func_stash   ; 0b01111
-        dd func_add     ; 0b10000
-        dd func_sub     ; 0b10001
-        dd func_mul     ; 0b10010
-        dd func_div     ; 0b10011
-        dd func_mod     ; 0b10100
-        dd func_and     ; 0b10101
-        dd func_or      ; 0b10110
-        dd func_xor     ; 0b10111
-        dd func_shift   ; 0b11000
-        dd func_ldb     ; 0b11001
-        dd func_ldw     ; 0b11010
-        dd func_stb     ; 0b11011
-        dd func_stw     ; 0b11100
-        dd func_srel    ; 0b11101
-        dd func_sbp     ; 0b11110
-        dd func_native  ; 0b11111
+        dq func_nop     ; 0b00000
+        dq func_lit     ; 0b00001
+        dq func_sstack  ; 0b00010
+        dq func_drop    ; 0b00011
+        dq func_dup     ; 0b00100
+        dq func_swap    ; 0b00101
+        dq func_over    ; 0b00110
+        dq func_rot     ; 0b00111
+        dq func_eq      ; 0b01000
+        dq func_not     ; 0b01001
+        dq func_gth     ; 0b01010
+        dq func_lth     ; 0b01011
+        dq func_jmp     ; 0b01100
+        dq func_jmpc    ; 0b01101
+        dq func_call    ; 0b01110
+        dq func_stash   ; 0b01111
+        dq func_add     ; 0b10000
+        dq func_sub     ; 0b10001
+        dq func_mul     ; 0b10010
+        dq func_div     ; 0b10011
+        dq func_mod     ; 0b10100
+        dq func_and     ; 0b10101
+        dq func_or      ; 0b10110
+        dq func_xor     ; 0b10111
+        dq func_shift   ; 0b11000
+        dq func_ldb     ; 0b11001
+        dq func_ldw     ; 0b11010
+        dq func_stb     ; 0b11011
+        dq func_stw     ; 0b11100
+        dq func_srel    ; 0b11101
+        dq func_sbp     ; 0b11110
+        dq func_native  ; 0b11111
 
 func_nop:
-	cmp byte [vm.flag_rs], 1
+	test r11, MASK_ALT
 	jne .hlt
 	ret
 .hlt:	cli
@@ -273,248 +297,238 @@ func_nop:
 	jmp .hlt
 
 func_lit:
-	lodsd
-	push_ps eax
+	lodsq
+	push_ps rax
 	ret
 
 func_sstack:
-	lodsd
-	mov ebx, ebp
-	mov ebp, eax
-	push_ps ebx
+	lodsq
+	mov rbx, r8
+	mov r8, tax
+	push_ps rbx
 	ret
 
 func_drop:
-	pop_ps eax
+	pop_ps rax
 	ret
 
 func_dup:
-	pop_ps eax
-	push_ps eax
-	push_ps eax
+	pop_ps rax
+	push_ps rax
+	push_ps rax
 	ret
 
 func_swap:
-	pop_ps ebx
-	pop_ps eax
-	push_ps ebx
-	push_ps eax
+	pop_ps rbx
+	pop_ps rax
+	push_ps rbx
+	push_ps rax
 	ret
 
 func_over:
-	pop_ps ebx
-	pop_ps eax
-	push_ps eax
-	push_ps ebx
-	push_ps eax
+	pop_ps rbx
+	pop_ps rax
+	push_ps rax
+	push_ps rbx
+	push_ps rax
 	ret
 
 func_rot:
-	pop_ps ecx
-	pop_ps ebx
-	pop_ps eax
-	push_ps ebx
-	push_ps ecx
-	push_ps eax
+	pop_ps rcx
+	pop_ps rbx
+	pop_ps rax
+	push_ps rbx
+	push_ps rcx
+	push_ps rax
 	ret
 
 func_eq:
-	pop_ps ebx
-	pop_ps eax
-	cmp eax, ebx
-	call eqtoeax
-	push_ps eax
+	pop_ps rbx
+	pop_ps rax
+	cmp rax, rbx
+	call eqtorax
+	push_ps rax
 	ret
 
 func_not:
-	pop_ps eax
-	xor eax, 1
-	push_ps eax
+	pop_ps rax
+	xor rax, 1
+	push_ps rax
 	ret
 
 func_gth:
-	pop_ps ebx
-        pop_ps eax
-        cmp eax, ebx
-        call abovetoeax
-        push_ps eax
+	pop_ps rbx
+        pop_ps rax
+        cmp rax, rbx
+        call abovetorax
+        push_ps rax
         ret
 
 func_lth:
-        pop_ps ebx
-        pop_ps eax
-        cmp eax, ebx
-        call belowtoeax
-        push_ps eax
+        pop_ps rbx
+        pop_ps rax
+        cmp rax, rbx
+        call belowtorax
+        push_ps rax
         ret
 
 func_jmp:
-	pop_ps esi
+	pop_ps rsi
 	ret
 
 func_jmpc:
-	pop_ps ebx
-	pop_ps eax
+	pop_ps rbx
+	pop_ps rax
 
-	cmp eax, 0
+	cmp rax, 0
 	je .ret
 
-	mov esi, ebx
+	mov rsi, rbx
 
 .ret:	ret
 
 func_call:
-	push_rs esi
-	pop_ps esi
+	push_rs rsi
+	pop_ps rsi
 	ret
 
 func_stash:
-	pop_ps eax
-	push_rs eax
+	pop_ps rax
+	push_rs rax
 	ret
 
 func_add:
-	pop_ps ebx
-	pop_ps eax
-	add eax, ebx
-	push_ps eax
+	pop_ps rbx
+	pop_ps rax
+	add rax, rbx
+	push_ps rax
 	ret
 
 func_sub:
-        pop_ps ebx
-        pop_ps eax
-        sub eax, ebx
-        push_ps eax
+        pop_ps rbx
+        pop_ps rax
+        sub rax, rbx
+        push_ps rax
         ret
 
 func_mul:
-	; protect edx
-	mov ecx, edx
-	xor edx, edx
+	xor rdx, rdx
 
-	pop_ps ebx
-	pop_ps eax
+	pop_ps rbx
+	pop_ps rax
 
-	mul ebx
+	mul rbx
 
-	push_ps eax
-
-	; unprotect edx
-	mov edx, ecx
+	push_ps rax
 	ret
 
 func_div:  
-        ; protect edx
-        mov ecx, edx
-        xor edx, edx
+        xor rdx, rdx
 
-        pop_ps ebx
-        pop_ps eax
+        pop_ps rbx
+        pop_ps rax
 
-        div ebx
+        div rbx
 
-        push_ps eax
-
-        ; unprotect edx
-        mov edx, ecx
+        push_ps rax
         ret
 
 func_mod:  
-        ; protect edx
-        mov ecx, edx
-        xor edx, edx
+        xor rdx, rdx
 
-        pop_ps ebx
-        pop_ps eax
+        pop_ps rbx
+        pop_ps rax
 
-        div ebx
+        div rbx
 
-        push_ps edx
-
-        ; unprotect edx
-        mov edx, ecx
+        push_ps rdx
         ret
 
 func_and:
-	pop_ps ebx
-	pop_ps eax
-	and eax, ebx
-	push_ps eax
+	pop_ps rbx
+	pop_ps rax
+	and rax, rbx
+	push_ps rax
 	ret
 
 func_or:
-        pop_ps ebx
-        pop_ps eax
-        or eax, ebx
-        push_ps eax
+        pop_ps rbx
+        pop_ps rax
+        or rax, rbx
+        push_ps rax
         ret
 
 func_xor:
-        pop_ps ebx
-        pop_ps eax
-        xor eax, ebx
-        push_ps eax
+        pop_ps rbx
+        pop_ps rax
+        xor rax, rbx
+        push_ps rax
         ret
 
 func_shift:
-	pop_ps ebx
-	pop_ps eax
+	pop_ps rbx
+	pop_ps rax
 
-	mov ecx, ebx
-	and ecx, 0x7fffffff
+	mov rcx, rbx
+	and rcx, 0x7fffffffffffffff
 
-	test ebx, 0x80000000
+	test rbx, 0x800000000000000000
 	je .shr
 
-	shl eax, cl
+	shl rax, cl
 	jmp .ret
-.shr:	shr eax, cl
+.shr:	shr rax, cl
 
-.ret:	push_ps eax
+.ret:	push_ps rax
 	ret
 
 func_ldb:
-	xor eax, eax
-	pop_ps ebx
-	mov al, byte [ebx]
-	push_ps eax
+	xor rax, rax
+	pop_ps rbx
+	mov al, byte [rbx]
+	push_ps rax
 	ret
 
 func_ldw:
-	pop_ps ebx
-	mov eax, dword [ebx]
-	push_ps eax
+	pop_ps rbx
+	mov rax, qword [rbx]
+	push_ps rax
 	ret
 
 func_stb:
-	pop_ps ebx
-	pop_ps eax
+	pop_ps rbx
+	pop_ps rax
 
-	mov byte [ebx], al
+	mov byte [rbx], al
 	ret
 
 func_stw:
-	pop_ps ebx
-	pop_ps eax
+	pop_ps rbx
+	pop_ps rax
 
-	mov dword [ebx], eax
+	mov qword [rbx], rax
 	ret
 
 func_srel:
-	pop_ps eax
-	add eax, edx
-	push_ps eax
+	pop_ps rax
+
+	test r11, MASK_ALT
+	jne .n
+.p:	add rax, rsi
+	jmp .r
+.n:	add rax, r10
+.r:	push_ps rax
 	ret
 
 func_sbp:
-	mov eax, edx
-	pop_ps edx
-	add edx, ebp
-	push_ps eax
+	mov rax, r10
+	pop_ps r10
+	add r10, r8
+	push_ps rax
 	ret
 
 func_native:
 	ret
-	pop_ps eax
-	call eax
+	pop_ps rax
+	call rax
 	ret
